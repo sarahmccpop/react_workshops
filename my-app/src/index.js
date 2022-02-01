d5b8172571577386802f784cd2c5d3f753c952dd
Sarah McConville <sarah.mcconville@wegotpop.com>
2022-02-01 18:10:49 +0000

    js is syntactic sugar

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+  // jsx is just a syntactic sugar for react.createElement
+  <App />,
+  // e.g. this React.createElement(App, null, null), is the same as above
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
